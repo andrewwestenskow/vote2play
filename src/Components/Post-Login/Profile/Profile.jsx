35ca7440d4835632ee796f0315581db5eb281369
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-05-10 16:23:44 -0600

    Updated playlist component

--- src/Components/Post-Login/Profile/Profile.jsx
+++ src/Components/Post-Login/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import EditForm from './EditForm/EditForm'
+import YouTube from 'react-youtube'
 
 class Profile extends Component {
 
@@ -18,10 +19,28 @@ class Profile extends Component {
     let userGroups = await axios.get('/api/group/getgroups')
     axios.get('/api/users/info').then(res => {
       let userInfo = res.data[0]
+
+      function YouTubeGetID(url){
+        var ID = '';
+        url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        if(url[2] !== undefined) {
+          //eslint-disable-next-line
+          ID = url[2].split(/[^0-9a-z_\-]/i);
+          ID = ID[0];
+        }
+        else {
+          ID = url;
+        }
+          return ID;
+      }
+
+      let faveSong = YouTubeGetID(userInfo.favoritesong)
+
       this.setState({
         firstname: userInfo.firstname,
         lastname: userInfo.lastname,
         favoritesong: userInfo.favoritesong,
+        favoritesongid: faveSong,
         image: userInfo.image,
       })
     })
@@ -57,21 +76,22 @@ class Profile extends Component {
       return <div key={group.group_id}>{group.name}</div>
     })
 
-    let { firstname, lastname, favoritesong, image } = this.state
+    let { firstname, lastname, image } = this.state
     return (
       <div>Profile
 
         {!this.state.loading &&
           <div>
             <button onClick={this.toggleEdit}>Edit info</button>
+            {this.state.edit && <EditForm editToggle={this.editToggle} sendForm={this.sendForm}
+            userInfo={this.state}/>}
             <div>
               {firstname}{lastname}
               <img src={image} alt={firstname} />
-              {favoritesong}
+              <h1>Favorite Song: </h1>
+              <YouTube videoId={this.state.favoritesongid}/>
             </div>
             
-            {this.state.edit && <EditForm editToggle={this.editToggle} sendForm={this.sendForm}
-            userInfo={this.state}/>}
 
             <div>
               <h1>GROUPS:</h1>
