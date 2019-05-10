8e4405884f661b9db9ba5a1c1d9a07617846a52e
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-05-10 17:38:00 -0600

    Image upload for group works

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const app = express()
 const massive = require('massive')
 const session = require('express-session')
 const socket = require('socket.io')
+const aws = require('aws-sdk')
 const UsersCtrl = require('./Controllers/UsersController')
 const GroupCtrl = require('./Controllers/GroupController')
 const AuthCtrl = require('./Controllers/AuthController')
 const PlaylistCtrl = require('./Controllers/PlaylistController')
 const authMiddleware = require('./Middlewares/authMiddleware')
 
-const{SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+const{SERVER_PORT, SESSION_SECRET, CONNECTION_STRING, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
 
 
 //MIDDLEWARE
@@ -77,6 +78,40 @@ app.post('/api/playlist/addback', PlaylistCtrl.addBack)
 //USERS ENDPOINTS
 app.get('/api/users/info', UsersCtrl.getUserInfo)
 app.put('/api/users/info', UsersCtrl.updateInfo)
+
+// AWS S3 ENDPOINTS
+app.get('/sign-s3', (req, res) => {
+
+  aws.config = {
+    region: 'us-west-1',
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+  
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+
+    return res.send(returnData)
+  });
+});
 
 
 //MASSIVE CONNECTION
--- src/Components/Post-Login/CreateGroup/CreateGroup.jsx
+++ src/Components/Post-Login/CreateGroup/CreateGroup.jsx
@@ -1,19 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import {withRouter} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Dropzone from 'react-dropzone'
+import { GridLoader } from 'react-spinners'
+import randomString from 'random-string'
 
-class CreateGroup extends Component{
+class CreateGroup extends Component {
 
-  state={
+  state = {
     groupName: '',
-    groupImage: ''
+    groupImage: '',
+    isUploading: false
   }
+
+  uploadFile = (file, signedRequest, url) => {
+    const options = {
+      headers: {
+        'Content-Type': file.type,
+      },
+    };
+
+    axios
+      .put(signedRequest, file, options)
+      .then(response => {
+        this.setState({ isUploading: false, url });
+        // THEN DO SOMETHING WITH THE URL. SEND TO DB USING POST REQUEST OR SOMETHING
+      })
+      .catch(err => {
+        this.setState({
+          isUploading: false,
+        });
+        if (err.response.status === 403) {
+          alert(
+            `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
+            err.stack
+            }`
+          );
+        } else {
+          alert(`ERROR: ${err.status}\n ${err.stack}`);
+        }
+      });
+  };
 
   handleCreateGroup = async (e) => {
     e.preventDefault()
-    const {groupName, groupImage} = this.state
-    const {login_id} = this.props
+    const { groupName, groupImage } = this.state
+    const { login_id } = this.props
 
     let body = {
       name: groupName,
@@ -38,8 +71,29 @@ class CreateGroup extends Component{
     })
   }
 
-  render(){
-    return(
+  getSignedRequest = ([file]) => {
+    this.setState({ isUploading: true })
+
+    const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`
+
+    axios.get('/sign-s3', {
+      params: {
+        'file-name': fileName,
+        'file-type': file.type
+      }
+    }).then((response) => {
+      const { signedRequest, url } = response.data
+      this.uploadFile(file, signedRequest, url)
+      this.setState({
+        groupImage: url
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  render() {
+    return (
       <div className='Create-Group'>Create GROUP
 
         <form onSubmit={this.handleCreateGroup}>
@@ -47,6 +101,43 @@ class CreateGroup extends Component{
           <input type="text" name='groupName' onChange={this.handleCreateGroupFormUpdate} />
           <p>Group Image</p>
           <input type="text" name='groupImage' onChange={this.handleCreateGroupFormUpdate} />
+
+          <Dropzone
+            onDropAccepted={this.getSignedRequest}
+            accept='image/*'
+            multiple={false} >
+
+            {({ getRootProps, getInputProps }) => (
+
+              <section className="container">
+                <div {...getRootProps({ className: 'dropzone' })}>
+                  <input {...getInputProps()} />
+                  <div style={{
+                    position: 'relative',
+                    width: 200,
+                    height: 200,
+                    borderWidth: 7,
+                    marginTop: 100,
+                    borderColor: 'rgb(102, 102, 102)',
+                    borderStyle: 'dashed',
+                    borderRadius: 5,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: 28,
+                  }} className='File-Drop'>
+                    {this.state.isUploading
+                      ? <GridLoader />
+                      : <p>Drop File or Click Here</p>
+                    }
+                  </div>
+                </div>
+              </section>
+
+            )}
+
+          </Dropzone>
+
           <button>Create Group</button>
         </form>
       </div>
@@ -55,8 +146,8 @@ class CreateGroup extends Component{
 }
 
 const mapStateToProps = (reduxState) => {
-  const {login_id} = reduxState.users
-  return{
+  const { login_id } = reduxState.users
+  return {
     login_id
   }
 }
