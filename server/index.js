82324a6a5b1d8318f4fb34703c83cd7267b5531e
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-05-20 14:21:36 -0600

    Testing sockets

--- server/index.js
+++ server/index.js
@@ -31,6 +31,8 @@ app.use(express.static(`${__dirname}/../build`))
 
 
 const server = app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
+
+
 const io = socket(server)
 
 io.on('connection', socket => {
